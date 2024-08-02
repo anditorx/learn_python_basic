80c602f7d20c855e324ae7db5f85dccd38e08ace
Andi Rustianto <anditodev46@gmail.com>
2024-08-02 13:17:14 +0700

    init

---
+++ 01_variableAndDataType.py
@@ -0,0 +1,18 @@
+# variable
+nama = "Budi"
+print(nama)
+
+nama = "Dante"
+print(nama)
+print(type(nama))
+
+#data type in python
+# Text Type = string
+# Number Type = Integer, Float, Hexdecimal
+# Binary Type = Boolean(true or false)
+# Sequence Type = List, Tuple, Set, and Dictionary
+
+arList = [1,2,3,4]
+print(arList)
+arList = ["satu","dua","tiga","empat"]
+print(arList)
---
+++ 02_operator.py
@@ -0,0 +1,18 @@
+def luas_persegi_panjang(panjang, lebar):
+    return panjang * lebar
+
+# Contoh penggunaan
+panjang = 10
+lebar = 5
+luas = luas_persegi_panjang(panjang, lebar)
+print(f"Luas persegi panjang dengan panjang {panjang} dan lebar {lebar} adalah {luas}")
+
+
+def luas_segitiga(alas, tinggi):
+    return 0.5 * alas * tinggi
+
+# Contoh penggunaan
+alas = 8
+tinggi = 5
+luas = luas_segitiga(alas, tinggi)
+print(f"Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luas}")
