d0fca20af930e5bc33854cb2e40b557f1b55613e
Andi Rustianto <anditodev46@gmail.com>
2024-08-05 23:00:58 +0700

    molule in python

---
+++ 03_09_module.py
@@ -0,0 +1,7 @@
+import module_playlist as pl
+
+print(pl.GENRE)
+pl.play("Digimon - Butterfly")
+pl.pause("Digimon - Butterfly")
+
+
---
+++ module_playlist.py
@@ -0,0 +1,9 @@
+GENRE = "Anime"
+DURATION = "Short"
+TYPE = "Cheerful"
+
+def play(song):
+    print("Is now playing - " + song)
+    
+def pause(song):
+    print("Pause - " + song)
